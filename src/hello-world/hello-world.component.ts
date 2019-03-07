97476029bd32d102cfcd1ac98e5cc14bc5212b0a
Greg Magolan <gmagolan@gmail.com>
2019-03-06 18:49:36 -0800

    WIP

--- src/hello-world/hello-world.component.ts
+++ src/hello-world/hello-world.component.ts
@@ -4,7 +4,7 @@ import {Component, NgModule} from '@angular/core';
 @Component({
   selector: 'hello-world-app',
   template: `
-    <div>Hello {{ name }}!</div>
+    <div id="greeting">Hello {{ name }}</div>
     <input type="text" [value]="name" (input)="name = $event.target.value"/>
   `,
   // TODO: might be better to point to .scss so this looks valid at design-time
